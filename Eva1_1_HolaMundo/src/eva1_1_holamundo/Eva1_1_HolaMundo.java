5f7425023bbbf483d8340909320cf2a48eba35f5
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-02-07 09:40:36 -0600

    Add files via upload

---
+++ Eva1_1_HolaMundo/src/eva1_1_holamundo/Eva1_1_HolaMundo.java
@@ -0,0 +1,12 @@
+
+package eva1_1_holamundo;
+
+/*
+ * DAVID
+ */
+public class Eva1_1_HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
