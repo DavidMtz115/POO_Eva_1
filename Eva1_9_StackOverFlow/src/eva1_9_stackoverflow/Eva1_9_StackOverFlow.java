beaf9354936e8cf757bd20b436dfbe3ff2e93a51
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-02-22 09:34:30 -0600

    Add files via upload

---
+++ Eva1_9_StackOverFlow/src/eva1_9_stackoverflow/Eva1_9_StackOverFlow.java
@@ -0,0 +1,15 @@
+
+package eva1_9_stackoverflow;
+
+/*
+ David
+ */
+public class Eva1_9_StackOverFlow {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+}
