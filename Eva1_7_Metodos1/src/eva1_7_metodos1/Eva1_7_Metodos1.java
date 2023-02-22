809dd2de9691776d0a264e64ea67d7780a471d7f
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-02-22 09:17:24 -0600

    Add files via upload

---
+++ Eva1_7_Metodos1/src/eva1_7_metodos1/Eva1_7_Metodos1.java
@@ -0,0 +1,16 @@
+
+package eva1_7_metodos1;
+
+/*
+ David
+ */
+public class Eva1_7_Metodos1 {
+
+    public static void main(String[] args) {
+        
+        myMethod();//invocar al metodo(llamada a funcion)
+    }
+    public static void myMethod(){
+        System.out.println("You have called me; my name is myMethod!");
+    }
+}
