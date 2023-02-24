464d376891d97dc04cc8ed2f373dce17348f1d8b
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-02-24 09:09:12 -0600

    Add files via upload

---
+++ Eva1_15_Sobrecarga/src/eva1_15_sobrecarga/Eva1_15_Sobrecarga.java
@@ -0,0 +1,19 @@
+
+package eva1_15_sobrecarga;
+
+/*
+ David
+ */
+public class Eva1_15_Sobrecarga {
+
+    public static void main(String[] args) {
+        /*Diferentes println: son 10 diferentes
+        metodos, para imprimir diferentes tipos de datos*/
+        /*Para crear sobrecarga de metos la firma del metodo debe de ser unica*/
+        System.out.println("Hola mundo");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
