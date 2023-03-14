d63b2e0c610f8e0f3dec745673d5a364b898262c
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-03-13 21:47:28 -0600

    Add files via upload

---
+++ Eva2_5_TiposHerencia/src/eva2_5_tiposherencia/Eva2_5_TiposHerencia.java
@@ -0,0 +1,25 @@
+
+package eva2_5_tiposherencia;
+
+/*
+ David
+ */
+public class Eva2_5_TiposHerencia {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+class SuperClase{
+    
+}
+
+class OtraSuperClase{//no se puede heredar de dos o mas superclases
+    
+}
+
+class BaseClase extends SuperClase{//eztends es solo para 1 clase
+    
+}
