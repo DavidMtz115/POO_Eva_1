ee04b6dcd7a9705763023d8ca1ee5f66ff9cb407
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-03-21 21:07:25 -0600

    Add files via upload

---
+++ Eva2_12_ClasesAbstractas/src/eva2_12_clasesabstractas/Eva2_12_ClasesAbstractas.java
@@ -0,0 +1,23 @@
+
+package eva2_12_clasesabstractas;
+
+/*
+ David
+ */
+public class Eva2_12_ClasesAbstractas {
+
+    public static void main(String[] args) {
+        
+        Prueba pru = new Prueba();
+        //PruebaAbstracta abs = new PruebaAbstracta();
+        //No se pueden crear objetos de clases abstractas
+    }
+}
+
+class Prueba{
+    
+}
+
+abstract class PruebaAbstracta{
+    
+}
