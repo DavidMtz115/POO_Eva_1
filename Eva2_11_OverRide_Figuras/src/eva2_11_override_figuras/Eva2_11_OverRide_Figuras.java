2c31bd6036e6d180f4a24400654cd3ba2e4a77e5
DavidMtz115 <124702593+DavidMtz115@users.noreply.github.com>
2023-03-21 21:07:00 -0600

    Add files via upload

---
+++ Eva2_11_OverRide_Figuras/src/eva2_11_override_figuras/Eva2_11_OverRide_Figuras.java
@@ -0,0 +1,18 @@
+
+package eva2_11_override_figuras;
+
+/*
+ David
+ */
+public class Eva2_11_OverRide_Figuras {
+
+    public static void main(String[] args) {
+        
+        Circulo cir = new Circulo(10);
+        System.out.println(cir + "\n");
+        
+        Triangulo tri = new Triangulo(5, 6, 5);
+        System.out.println(tri);
+    }
+    
+}
---
+++ Eva2_11_OverRide_Figuras/src/eva2_11_override_figuras/Figura.java
@@ -0,0 +1,15 @@
+
+package eva2_11_override_figuras;
+
+/*
+ David
+ */
+public class Figura {
+ 
+    public double calcularArea(){
+        return -1;
+    }
+    public double calcularPeri(){
+        return -1;
+    }
+}
